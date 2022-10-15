9f1670ba2762689098398f33664b8dda230dd4a1
Nara Tekchuen <52786849+Nartek01@users.noreply.github.com>
2022-10-15 15:52:18 +0200

    Added routes preparing for the checkout basket

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import { Header } from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header />
-      <Home />
-      {/* Footer */}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route>
+            <Header />
+            <h1>I AM A CHECKOUT</h1>
+          </Route>
+          <Route path={"/"}>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
