8df2a96e9892bff5f78685eb68ad2d3f2b83b971
Nara Tekchuen <52786849+Nartek01@users.noreply.github.com>
2022-10-15 21:08:12 +0200

    Moved checkout__left to have 2/3 desktop estate

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,23 +3,22 @@ import "./Checkout.css";
 
 function Checkout() {
   return (
-    <div>
-      <div className="checkout">
-        <div className="checkout__left">
-          <img
-            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-            alt="ad banner"
-            className={"checkout__ad"}
-          />
-        </div>
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="ad banner"
+          className={"checkout__ad"}
+        />
 
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
+          {/*    BasketItem*/}
         </div>
+      </div>
 
-        <div className="checkout__right">
-          <h2>The subtotal will go here</h2>
-        </div>
+      <div className="checkout__right">
+        <h2>The subtotal will go here</h2>
       </div>
     </div>
   );
