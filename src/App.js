373d805d93b3bfc88eda75997dc811c20476011d
Nara Tekchuen <52786849+Nartek01@users.noreply.github.com>
2022-10-15 13:50:11 +0200

    insert header component to the root

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Header } from './Header';
 
 function App() {
   return (
+      // BEM
     <div className="App">
-      <h1>Hello World</h1>
+        <Header/>
+    {/* Home */}
+    {/* Footer */}
     </div>
   );
 }
