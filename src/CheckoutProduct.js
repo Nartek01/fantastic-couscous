7c59e1af4c9398aac88baa497f279494fac315a7
Nara <nartek01@gmail.com>
2022-10-31 23:25:20 +0100

    Imported props, and mapped them to corresponding div for rendering.

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CheckoutProduct.css";
+
+function CheckoutProduct(id, image, title, price, rating) {
+    return (
+        <div className="checkoutProduct">
+            <img src={image} className={"checkouProduct__image"} alt="product images"/>
+            <div className="checkoutProduct__info">
+                <p className={"checkoutProduct__title"}>{title}</p>
+                <p className={"checkoutProduct__price"}>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                    <div className="checkoutProduct__rating">
+                        {Array(rating)
+                            .fill()
+                            .map(() => (
+                                <p>🌟</p>
+                            ))}
+                    </div>
+                </p>
+            </div>
+            <button>Remove from Basket</button>
+        </div>
+    )
+}
+
+export default CheckoutProduct;
